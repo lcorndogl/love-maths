910aa5e0fbe49a0884278202f534c0d2108582f2
Michael Cornall <mik.reds.forever@gmail.com>
2024-11-22 11:38:44 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+function runGame() {
+    
+}
+
+function checkAnswer() {
+    
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongAnswer() {
+    
+}
+
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
+function displayDivideQuestion() {
+
+}
+
+/*
+function add()
+
+function subtract()
+
+function multiply()
+
+function divide()*/
